b269a69bb4320fcf9c53a7e4b7d934bd542a6d8d
liaoxf <liaoxf@asiainfo.com>
2019-08-06 14:39:57 +0800

    mini image

--- src/aif.io/hubx/build/alpine_make.sh
+++ src/aif.io/hubx/build/alpine_make.sh
@@ -2,4 +2,4 @@
 
 . "$HOME/apps/hubx/src/aif.io/hubx/bin/Env.sh"
 
-docker run -it -v $GOPATH:$GOPATH demo/go-build:1.0 sh -c "$WORKROOT/build/make.sh"
+docker run -it -v $GOPATH:$GOPATH iseex.picp.io:30500/hubx/go-build:1.0 sh -c "$WORKROOT/build/make.sh"
